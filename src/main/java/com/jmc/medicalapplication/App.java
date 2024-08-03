21a38e010d7edd704c35c2fe08666ece48e83bda
Yaroslav <gamerplay0606@gmail.com>
2024-08-03 10:56:12 +0300

    #upDate

--- src/main/java/com/jmc/medicalapplication/App.java
+++ src/main/java/com/jmc/medicalapplication/App.java
@@ -1,4 +1,16 @@
 package com.jmc.medicalapplication;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
