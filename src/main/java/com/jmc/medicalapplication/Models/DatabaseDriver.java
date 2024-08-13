856b8ccdf29d12cc1e1d43c1ce597abae873cb4a
Yaroslav <gamerplay0606@gmail.com>
2024-08-13 17:01:22 +0300

    #update0.5

--- src/main/java/com/jmc/medicalapplication/Models/DatabaseDriver.java
+++ src/main/java/com/jmc/medicalapplication/Models/DatabaseDriver.java
@@ -1,4 +1,17 @@
 package com.jmc.medicalapplication.Models;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseDriver {
+    private Connection connection;
+
+    public DatabaseDriver(){
+        try {
+            this.connection = DriverManager.getConnection("jdbc:sqlite:medicalworkers.db");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/jmc/medicalapplication/Models/Model.java
+++ src/main/java/com/jmc/medicalapplication/Models/Model.java
@@ -1,4 +1,27 @@
 package com.jmc.medicalapplication.Models;
 
+import com.jmc.medicalapplication.Views.ViewFactory;
+
 public class Model {
+    private static Model model;
+    private final ViewFactory viewFactory;
+    private final DatabaseDriver databaseDriver;
+
+    private Model(){
+        this.viewFactory = new ViewFactory();
+        this.databaseDriver = new DatabaseDriver();
+    }
+
+    public static synchronized Model getInstance(){
+        if(model == null){
+            model = new Model();
+        }
+        return model;
+    }
+
+    public ViewFactory getViewFactory() {return viewFactory;}
+
+    public DatabaseDriver getDatabaseDriver() {return databaseDriver;}
+
+
 }
