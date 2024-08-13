06f4b99a7046aafe3f6f8f10dbc554e6db4f704f
Yaroslav <gamerplay0606@gmail.com>
2024-08-13 11:04:28 +0300

    #update0.3

--- src/main/java/com/jmc/medicalapplication/Controller/Admin/AddWorkerController.java
+++ src/main/java/com/jmc/medicalapplication/Controller/Admin/AddWorkerController.java
@@ -1,6 +1,7 @@
 package com.jmc.medicalapplication.Controller.Admin;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class AddWorkerController {
@@ -9,4 +10,6 @@ public class AddWorkerController {
     public TextField lname_fld;
     public TextField startJob_fld;
     public Button add_user_btn;
+    public TextField position_fld;
+    public Label status_lbl;
 }
