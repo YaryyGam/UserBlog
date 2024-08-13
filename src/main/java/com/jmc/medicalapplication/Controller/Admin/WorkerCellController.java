5446756efc73518e71fcf8daf68b36e2e779ef54
Yaroslav <gamerplay0606@gmail.com>
2024-08-13 17:35:08 +0300

    #update0.6

--- src/main/java/com/jmc/medicalapplication/Controller/Admin/WorkerCellController.java
+++ src/main/java/com/jmc/medicalapplication/Controller/Admin/WorkerCellController.java
@@ -1,13 +1,34 @@
 package com.jmc.medicalapplication.Controller.Admin;
 
+import com.jmc.medicalapplication.Models.Worker;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class WorkerCellController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WorkerCellController implements Initializable {
     public Label date_lbl;
     public Label fname_lbl;
     public Label sname_lbl;
     public Label lname_lbl;
     public Label time_lbl;
     public TextField hours_fld;
+
+    private final Worker worker;
+
+    public WorkerCellController(Worker worker){this.worker = worker;}
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        date_lbl.textProperty().bind(worker.dateProperty().asString());
+        fname_lbl.textProperty().bind(worker.lNameProperty());
+        sname_lbl.textProperty().bind(worker.sNameProperty());
+        lname_lbl.textProperty().bind(worker.lNameProperty());
+        time_lbl.textProperty().bind(worker.timeOfBeginProperty().asString());
+        hours_fld.setOnAction(e->{
+
+        });
+    }
 }
--- src/main/java/com/jmc/medicalapplication/Models/Worker.java
+++ src/main/java/com/jmc/medicalapplication/Models/Worker.java
@@ -1,4 +1,30 @@
 package com.jmc.medicalapplication.Models;
 
+import javafx.beans.property.*;
+
+import java.time.LocalDate;
+
 public class Worker {
+    private final StringProperty lName;
+    private final StringProperty fName;
+    private final StringProperty sName;
+    private final DoubleProperty timeOfBegin;
+    private final StringProperty currentPosition;
+    private final ObjectProperty<LocalDate> date;
+
+    public Worker(String lName, String fName, String sName, double timeOfBegin, String position, LocalDate date){
+        this.lName = new SimpleStringProperty(this, "Last Name", lName);
+        this.fName = new SimpleStringProperty(this, "First Name", fName);
+        this.sName = new SimpleStringProperty(this, "Second Name", sName);
+        this.timeOfBegin = new SimpleDoubleProperty(this, "Time Of Begin", timeOfBegin);
+        this.currentPosition = new SimpleStringProperty(this, "Position", position);
+        this.date = new SimpleObjectProperty<>(this, "Date", date);
+    }
+
+    public StringProperty lNameProperty() {return this.lName;}
+    public StringProperty fNameProperty() {return this.fName;}
+    public StringProperty sNameProperty() {return this.sName;}
+    public DoubleProperty timeOfBeginProperty() {return this.timeOfBegin;}
+    public StringProperty currentPositionProperty() {return this.currentPosition;}
+    public ObjectProperty<LocalDate> dateProperty() {return this.date;}
 }
