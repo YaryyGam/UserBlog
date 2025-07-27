497eeed0ba7c290886d2e1dad726ea42ad66faa8
Yaroslav <98101351+YaryyGam@users.noreply.github.com>
2025-07-27 03:18:09 +0300

    Delete JavaSpring directory

--- JavaSpring/Part 26 Quiz-service/quiz-service/src/main/java/com/yaryy/quiz_service/DAO/QuizDAO.java
+++
@@ -1,9 +0,0 @@
-package com.yaryy.quiz_service.DAO;
-
-import com.yaryy.quiz_service.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizDAO extends JpaRepository<Quiz, Integer> {
-}
